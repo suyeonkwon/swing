c66050db1fc98341e40b3b4c5293b480e68a6b08
hyelyun-y <68841090+hyelyun-y@users.noreply.github.com>
2020-08-26 14:44:15 +0900

    Update Classinfo.java

--- src/main/java/logic/Classinfo.java
+++ src/main/java/logic/Classinfo.java
@@ -1,5 +1,6 @@
 package logic;
 
+import java.util.List;
 
 public class Classinfo {
 	private int classid; // 수업 아이디
@@ -11,6 +12,8 @@ public class Classinfo {
 	private String place;
 	private String title;
 	private String curri;
+	private List<String> titlelist;
+	private List<String> currilist;
 
 	// getter, setter, toString
 	public int getClassid() {
@@ -68,6 +71,18 @@ public class Classinfo {
 		this.curri = curri;
 	}
 	
+	public List<String> getTitlelist() {
+		return titlelist;
+	}
+	public void setTitlelist(List<String> titlelist) {
+		this.titlelist = titlelist;
+	}
+	public List<String> getCurrilist() {
+		return currilist;
+	}
+	public void setCurrilist(List<String> currilist) {
+		this.currilist = currilist;
+	}
 	@Override
 	public String toString() {
 		return "Classinfo [classid=" + classid + ", classno=" + classno + ", classseq=" + classseq + ", date=" + date
